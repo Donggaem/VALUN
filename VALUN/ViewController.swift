ad25f5e473ab42f757cddf8e586b1e5e26cad350
Donggaem <ehdrua465111@naver.com>
2022-12-13 15:04:14 +0900

    Initial Commit

---
+++ VALUN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VALUN
+//
+//  Created by 김동겸 on 2022/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
