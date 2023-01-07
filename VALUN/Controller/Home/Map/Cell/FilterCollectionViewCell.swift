4243650a2f46f20dabb6e592c5c51d90ba0d7881
Donggaem <ehdrua465111@naver.com>
2023-01-07 22:12:49 +0900

    맵화면 카테고리 보기 화면 구현

--- VALUN/Controller/Home/Map/Cell/FilterCollectionViewCell.swift
+++ VALUN/Controller/Home/Map/Cell/FilterCollectionViewCell.swift
@@ -9,21 +9,45 @@ import UIKit
 
 class FilterCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet var filterView: UIView! {
+    
+    @IBOutlet var filterView: UIView!{
         didSet {
+            
             filterView.layer.cornerRadius = 14
-            filterView.layer.borderWidth = 1
-            filterView.layer.borderColor = UIColor(red: 0.587, green: 0.587, blue: 0.587, alpha: 1).cgColor
+            
+            
         }
     }
     @IBOutlet var filterTitle: UILabel!
     
-    @IBOutlet var filterBtn: UIButton! {
+    var cellBool = false {
         didSet {
-            
-            filterBtn.layer.cornerRadius = 14
-            filterBtn.layer.borderWidth = 1
-            filterBtn.layer.borderColor = UIColor(red: 0.587, green: 0.587, blue: 0.587, alpha: 1).cgColor
+            if cellBool == true {
+                filterView.backgroundColor = UIColor(red: 0.416, green: 0.769, blue: 0.478, alpha: 1)
+                filterTitle.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+            }else {
+                filterTitle.textColor =  UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
+                filterView.backgroundColor = UIColor(red: 0.933, green: 0.933, blue: 0.933, alpha: 1)
+            }
         }
     }
+    
+    override func awakeFromNib() {
+            super.awakeFromNib()
+        }
+    
+//    override var isSelected: Bool {
+//            didSet {
+//                if isSelected {
+//                    if cellBool == false {
+//                        
+//                        cellBool = true
+//                    }else {
+//                        
+//                        cellBool = false
+//
+//                    }
+//                }
+//            }
+//        }
 }
--- VALUN/Controller/Home/Map/MapViewController.swift
+++ VALUN/Controller/Home/Map/MapViewController.swift
@@ -12,6 +12,20 @@ class MapViewController: UIViewController {
     @IBOutlet var subView: UIView!
     var mapView: MTMapView?
     
+    @IBOutlet var naviView: UIView!
+    
+    @IBOutlet var categoryChoiceBtn: UIButton!
+    
+    @IBOutlet var filterView: UIView!
+    
+    @IBOutlet var filterViewConstraint: NSLayoutConstraint!
+    @IBOutlet var filter_OneCollectionView: UICollectionView!
+    
+    
+    @IBOutlet var filter_CancelBtn: UIButton!
+    
+    @IBOutlet var filter_ApplyBtn: UIButton!
+    
     @IBOutlet var listBtn: UIButton!
     
     @IBOutlet var modalView: UIView!
@@ -36,7 +50,8 @@ class MapViewController: UIViewController {
     var tabletestImg: [String] = ["common1", "common2", "common3"]
     
     @IBOutlet var filterCollectionView: UICollectionView!
-    var filtertest: [String] = ["전체", "일반쓰레기", "플라스틱", "유리", "종이", "박스"]
+    
+    var filterTuple: [(String, Bool)] = [("PET", false), ("금속", false), ("종이", false), ("플라스틱", false), ("일반쓰레기", false), ("스티로폼", false), ("유리", false), ("음식물 쓰레기", false), ("폐기물", false), ("목재", false), ("비닐", false), ("기타", false)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,16 +70,31 @@ class MapViewController: UIViewController {
     //MARK: - Inner Func
     private func setUI() {
         
+        //필터 초기화
+        removeFilter()
+        
         //네비바 숨김
         self.navigationController?.navigationBar.isHidden = true
         
         // 뷰를 최상위로
+        self.view.bringSubviewToFront(filterView)
         self.view.bringSubviewToFront(listBtn)
         self.view.bringSubviewToFront(modalBtn)
         self.view.bringSubviewToFront(modalView)
         self.view.bringSubviewToFront(listView)
-
+        
         //둥글게
+        
+        categoryChoiceBtn.layer.cornerRadius = 10
+        categoryChoiceBtn.layer.borderWidth = 1
+        categoryChoiceBtn.layer.borderColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1).cgColor
+        
+        filter_CancelBtn.layer.cornerRadius = 10
+        filter_CancelBtn.layer.borderWidth = 1
+        filter_CancelBtn.layer.borderColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1).cgColor
+        
+        filter_ApplyBtn.layer.cornerRadius = 10
+        
         listBtn.layer.cornerRadius = 16
         modalView.layer.cornerRadius = 10
         modalView.clipsToBounds = true
@@ -84,11 +114,53 @@ class MapViewController: UIViewController {
         
     }
     
+    //컬렉션뷰셀 클릭
+    func cellClick(index: Int) {
+        let newValue: (String, Bool) = (filterTuple[index].0, !filterTuple[index].1)
+        filterTuple[index] = newValue
+        filterCollectionView.reloadData()
+        filter_OneCollectionView.reloadData()
+    }
+    
+    //필터 초기화
+    func removeFilter() {
+        for index in 0..<filterTuple.endIndex {
+            if filterTuple.map{$0.1}[index] == true {
+                let changeValue: (String, Bool) = (filterTuple[index].0, !filterTuple[index].1)
+                filterTuple[index] = changeValue
+            }
+        }
+        filterCollectionView.reloadData()
+        filter_OneCollectionView.reloadData()
+    }
+    
     //MARK: - IBAction
     
     @IBAction func backBtnPressed(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
+    @IBAction func categoryChoiceBtnPressed(_ sender: UIButton) {
+
+        self.filterViewConstraint.constant = 0
+        
+    }
+    @IBAction func categoryDeleteBtnPressed(_ sender: UIButton) {
+        categoryChoiceBtn.isHidden = false
+        removeFilter()
+    }
+    
+    @IBAction func filter_CancelBtnPressed(_ sender: UIButton) {
+        
+        self.filterViewConstraint.constant = -281
+        removeFilter()
+
+    }
+    @IBAction func filter_ApplyBtnPressed(_ sender: UIButton) {
+        
+        self.filterViewConstraint.constant = -281
+        
+    }
+    
     @IBAction func listBtnPressed(_ sender: UIButton) {
         if listBtn.titleLabel?.text == "  목록보기" {
             self.listBtnBottomToListViewConstraint.isActive = true // 리스트버튼과 리스트뷰의 제약온
@@ -115,7 +187,7 @@ class MapViewController: UIViewController {
             self.listBtnBottomToListViewConstraint.constant = 64 //리스트버튼과 리스트뷰
             
             self.modalViewBottomConstraint.constant = -self.modalView.frame.height//모달뷰와 슈퍼뷰(모달을 열고 목록을 열고 닫고 했을때)
-
+            
             
             // 애니메이션
             UIView.animate(withDuration: 0.3, delay: 0, animations: {
@@ -133,7 +205,7 @@ class MapViewController: UIViewController {
     
     @IBAction func modalTest(_ sender: UIButton) {
         self.listBtnBottomToModalViewConstraint.isActive = true //리스트버튼과 모달뷰의 제약 온
-
+        
         //커스텀 모달뷰 방법
         // Bottom 제약조건 높이 조절
         self.modalViewBottomConstraint.constant = 56 //모달뷰와 슈퍼뷰
@@ -151,7 +223,7 @@ class MapViewController: UIViewController {
         UIView.animate(withDuration: 0.3, delay: 0, animations: {
             self.view.layoutIfNeeded()
         }, completion: nil)
-
+        
     }
     
     @IBAction func exitBtnPresed(_ sender: UIButton) {
@@ -199,7 +271,7 @@ extension MapViewController: MTMapViewDelegate {
             mapView.showCurrentLocationMarker = true
             
             self.view.addSubview(mapView)
-
+            
         }
     }
 }
@@ -210,22 +282,58 @@ extension MapViewController: UICollectionViewDataSource, UICollectionViewDelegat
     //필터
     // CollectionView 셋팅
     func setCollectionView() {
+        
         filterCollectionView.reloadData()
         filterCollectionView.delegate = self
         filterCollectionView.dataSource = self
         filterCollectionView.register(UINib(nibName: "FilterCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "FilterCollectionViewCell")
+        
+        filter_OneCollectionView.delegate = self
+        filter_OneCollectionView.dataSource = self
+        filter_OneCollectionView.register(UINib(nibName: "FilterCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "FilterCollectionViewCell")
     }
+
     
     // CollectionView item 개수
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return filtertest.count
+        var count = 0
+        if collectionView == filterCollectionView {
+            count = filterTuple.filter{$0.1 == true}.count
+            if count == 0 {
+                categoryChoiceBtn.isHidden = false
+
+            }else {
+                categoryChoiceBtn.isHidden = true
+
+            }
+
+        }else if collectionView == filter_OneCollectionView {
+            count = filterTuple.count
+
+        }
+        
+        return count
     }
     
     // CollectionView Cell의 Object
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FilterCollectionViewCell", for: indexPath) as! FilterCollectionViewCell
-                
-        cell.filterBtn.setTitle(filtertest[indexPath.row], for: .normal)
+        
+        if collectionView == filterCollectionView {
+            cell.filterTitle.text = filterTuple.filter{$0.1 == true}.map{$0.0}[indexPath.row]
+            cell.filterTitle.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+            cell.filterView.backgroundColor = UIColor(red: 0.416, green: 0.769, blue: 0.478, alpha: 1)
+            
+        }else if collectionView == filter_OneCollectionView {
+            
+            cell.filterTitle.text = filterTuple.map{$0.0}[indexPath.row]
+            cell.filterTitle.textColor =  UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
+            cell.filterView.backgroundColor = UIColor(red: 0.933, green: 0.933, blue: 0.933, alpha: 1)
+            cell.cellBool = filterTuple.map{$0.1}[indexPath.row]
+
+        }
+        
+
         
         return cell
     }
@@ -233,14 +341,26 @@ extension MapViewController: UICollectionViewDataSource, UICollectionViewDelegat
     // CollectionView Cell 터치
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 
+        cellClick(index: indexPath.row)
+        
     }
     
     // CollectionView Cell의 Size
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var size = CGSize()
         
-        let width: CGFloat = collectionView.frame.width / 5 - 1.0
+        if collectionView == filterCollectionView {
+            let width: CGFloat = collectionView.frame.width / 4 - 1.0
+            size = CGSize(width: width, height: 29)
+        } else if collectionView == filter_OneCollectionView {
+            
+            let width: CGFloat = collectionView.frame.width / 5 - 1.0
+            size = CGSize(width: width, height: 29)
+        }
         
-        return CGSize(width: width, height: 28)
+       
+        
+        return size
     }
     
     // CollectionView Cell의 위아래 간격
@@ -257,7 +377,7 @@ extension MapViewController: UICollectionViewDataSource, UICollectionViewDelegat
 
 //MARK: - TableViewExtension
 extension MapViewController: UITableViewDataSource, UITableViewDelegate{
-   
+    
     
     // TableView 셋팅
     private func setTableView() {
@@ -266,7 +386,7 @@ extension MapViewController: UITableViewDataSource, UITableViewDelegate{
         self.listTableView.register(UINib(nibName: "ListTableViewCell", bundle: nil),  forCellReuseIdentifier: "ListTableViewCell")
         
         listTableView.separatorStyle = UITableViewCell.SeparatorStyle.none //테이블뷰 셀 선 없애기
-
+        
     }
     
     
