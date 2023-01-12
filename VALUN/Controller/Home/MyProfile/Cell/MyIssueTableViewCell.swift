a00cafa175ff068f59aa7dfd406a20ec6ab5ad82
Donggaem <ehdrua465111@naver.com>
2023-01-12 14:02:35 +0900

    재가 제보한 이슈 전체보기탭 동작 로직 구현

--- VALUN/Controller/Home/MyProfile/Cell/MyIssueTableViewCell.swift
+++ VALUN/Controller/Home/MyProfile/Cell/MyIssueTableViewCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class MyIssueTableViewCell: UITableViewCell {
-
+    
     @IBOutlet var issueImage: UIImageView!{
         didSet {
             issueImage.layer.cornerRadius = 10
@@ -20,22 +20,43 @@ class MyIssueTableViewCell: UITableViewCell {
             issueStateView.layer.borderWidth = 1
             issueStateView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
             
-            
         }
     }
-    @IBOutlet var issueStateLabel: UILabel!
+    @IBOutlet var issueStateLabel: UILabel! 
     @IBOutlet var issueCategoryLabel: UILabel!
     @IBOutlet var issueContentLabel: UILabel!
+    
+    var status = "" {
+        didSet {
+            if issueStateLabel.text == "미해결" {
+                
+                issueStateLabel.textColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
+                issueStateView.layer.borderColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1).cgColor
+            }else if issueStateLabel.text == "해결중"  {
+                
+                issueStateLabel.textColor = UIColor(red: 0.929, green: 0.424, blue: 0.404, alpha: 1)
+                issueStateView.layer.borderColor = UIColor(red: 0.929, green: 0.424, blue: 0.404, alpha: 1).cgColor
+            }else if issueStateLabel.text == "해결완료"  {
+                
+                issueStateLabel.textColor =  UIColor(red: 0.416, green: 0.769, blue: 0.478, alpha: 1)
+                issueStateView.layer.borderColor = UIColor(red: 0.416, green: 0.769, blue: 0.478, alpha: 1).cgColor
+            }else if issueStateLabel.text == "신고됨"  {
+                
+                issueStateLabel.textColor =  UIColor(red: 0.486, green: 0.416, blue: 0.769, alpha: 1)
+                issueStateView.layer.borderColor = UIColor(red: 0.486, green: 0.416, blue: 0.769, alpha: 1).cgColor
+            }
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
--- VALUN/Controller/Home/MyProfile/I_Reported_Issue/Reported_AllIssueViewController.swift
+++ VALUN/Controller/Home/MyProfile/I_Reported_Issue/Reported_AllIssueViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import Alamofire
 
 class Reported_AllIssueViewController: UIViewController {
-
+    
     @IBOutlet var allIssueTableView: UITableView!
     
     var tabletest1: [String] = ["test1", "test2", "test3"]
@@ -21,12 +21,24 @@ class Reported_AllIssueViewController: UIViewController {
     var pendingsList: [WithSolution] = []
     var solvedsList: [WithSolution] = []
     
+    var allIssueList: [Issue] = []
+    
+    //키 영어 -> 밸류 한글
+   var categoryDictionary: Dictionary<String, String> = ["pet":"PET", "metal":"금속", "paper":"종이", "plastic":"플라스틱", "trash":"일반쓰레기", "styrofoam":"스티로폼", "glass":"유리", "garbage":"음식물 쓰레기", "waste":"폐기물", "lumber":"목재", "vinyl":"비닐", "etc":"기타"]
+    
+    var statusDictionary: Dictionary<String, String> = ["UNSOLVED":"미해결", "PENDING":"해결중", "SOLVED":"해결완료", "REPORTED":"신고됨"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        getMyAllIssue()
         setTableView()
     }
- 
+    
     //MARK: - GetMyAllIssue
     let header: HTTPHeaders = ["authorization": UserDefaults.standard.string(forKey: "token")!]
     private func getMyAllIssue() {
@@ -36,17 +48,34 @@ class Reported_AllIssueViewController: UIViewController {
                 guard let self = self else {return}
                 switch response.result {
                 case .success(let response):
-                        print(VALUNLog.debug("getMyAllIssue-success"))
-                    
+                    print(VALUNLog.debug("getMyAllIssue-success"))
+                    self.allIssueList.removeAll()
+                    self.unsolvedsList.removeAll()
+                    self.pendingsList.removeAll()
+                    self.solvedsList.removeAll()
+                    var sortList: [Issue] = []
                     if response.data != nil {
                         let data = response.data
                         self.unsolvedsList = data?.unsolveds ?? []
                         self.pendingsList = data?.pendings ?? []
                         self.solvedsList = data?.solveds ?? []
-                    }
                         
-
-
+                        for index in 0..<self.unsolvedsList.endIndex {
+                            sortList.append( self.unsolvedsList[index])
+                        }
+                        
+                        for index in 0..<self.pendingsList.endIndex {
+                            sortList.append( self.pendingsList[index].issue)
+                        }
+                        
+                        for index in 0..<self.solvedsList.endIndex {
+                            sortList.append( self.solvedsList[index].issue)
+                        }
+                        
+                    }
+                    self.allIssueList = sortList.sorted(by: {$0.id < $1.id })
+                    self.allIssueTableView.reloadData()
+                    
                 case .failure(let error):
                     VALUNLog.error("getMyAllIssue - err")
                     print(error.localizedDescription)
@@ -61,7 +90,7 @@ class Reported_AllIssueViewController: UIViewController {
 
 //MARK: - TableViewExtension
 extension Reported_AllIssueViewController: UITableViewDataSource, UITableViewDelegate{
-   
+    
     // TableView 셋팅
     private func setTableView() {
         self.allIssueTableView.delegate = self
@@ -69,43 +98,26 @@ extension Reported_AllIssueViewController: UITableViewDataSource, UITableViewDel
         self.allIssueTableView.register(UINib(nibName: "MyIssueTableViewCell", bundle: nil),  forCellReuseIdentifier: "MyIssueTableViewCell")
         
         allIssueTableView.separatorStyle = UITableViewCell.SeparatorStyle.none //테이블뷰 셀 선 없애기
-
+        
     }
     
     
     //Cell 갯수 반환
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let allListCount = unsolvedsList.count + pendingsList.count + solvedsList.count
-        return allListCount
+        return allIssueList.count
     }
     
     //각Row에서 해당하는 Cell을 Return하는 메소드
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyIssueTableViewCell", for: indexPath) as! MyIssueTableViewCell
         
-        cell.issueCategoryLabel.text = tabletest1[indexPath.row]
-        cell.issueContentLabel.text = tabletest2[indexPath.row]
-        cell.issueImage.image = UIImage(named: tabletestImg[indexPath.row])
-        cell.issueStateLabel.text = tabletest3[indexPath.row]
+        let url = URL(string: allIssueList[indexPath.row].imageUrl)
+        cell.issueImage.kf.setImage(with: url)
+        cell.issueCategoryLabel.text = categoryDictionary[allIssueList[indexPath.row].category]
+        cell.issueContentLabel.text = allIssueList[indexPath.row].description
+        cell.issueStateLabel.text = statusDictionary[allIssueList[indexPath.row].status]
         
-        if cell.issueStateLabel.text == "미해결" {
-            
-            cell.issueStateLabel.textColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
-            cell.issueStateView.layer.borderColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1).cgColor
-            
-        } else if cell.issueStateLabel.text == "해결중" {
-            
-            cell.issueStateLabel.textColor = UIColor(red: 0.929, green: 0.424, blue: 0.404, alpha: 1)
-            cell.issueStateView.layer.borderColor = UIColor(red: 0.929, green: 0.424, blue: 0.404, alpha: 1).cgColor
-            
-
-        }else if cell.issueStateLabel.text == "해결완료" {
-            
-            cell.issueStateLabel.textColor =  UIColor(red: 0.416, green: 0.769, blue: 0.478, alpha: 1)
-            cell.issueStateView.layer.borderColor = UIColor(red: 0.416, green: 0.769, blue: 0.478, alpha: 1).cgColor
-            
-
-        }
+        cell.status = statusDictionary[allIssueList[indexPath.row].status] ?? ""
         
         cell.selectionStyle = .none
         return cell
@@ -113,7 +125,42 @@ extension Reported_AllIssueViewController: UITableViewDataSource, UITableViewDel
     
     //클릭한 셀의 이벤트 처리
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       
-
+        // 클릭된 셀을 가져옴
+        let cell = tableView.cellForRow(at: indexPath) as! MyIssueTableViewCell
+        
+        if cell.issueStateLabel.text == "미해결" {
+            let storyBoard = UIStoryboard(name: "IssueDetail", bundle: nil)
+            let issueDetailVC = storyBoard.instantiateViewController(identifier: "IssueDetailViewController") as! IssueDetailViewController
+            self.navigationController?.pushViewController(issueDetailVC, animated: true)
+            
+            issueDetailVC.paramIssueObject.append(allIssueList[indexPath.row])
+            issueDetailVC.paramIssueDistance = ""
+            
+        }else if cell.issueStateLabel.text == "해결중" {
+            for index in 0..<pendingsList.endIndex {
+                if pendingsList[index].issue.id == allIssueList[indexPath.row].id {
+                    let storyBoard = UIStoryboard(name: "IssueValidation", bundle: nil)
+                    let issueValidationVC = storyBoard.instantiateViewController(identifier: "IssueValidationViewController") as! IssueValidationViewController
+                    self.navigationController?.pushViewController(issueValidationVC, animated: true)
+                    
+                    issueValidationVC.paramIssueObject.append(pendingsList[index])
+                }
+            }
+            
+            
+        }else if cell.issueStateLabel.text == "해결완료" {
+            for index in 0..<solvedsList.endIndex {
+                if solvedsList[index].issue.id == allIssueList[indexPath.row].id {
+                    let storyBoard = UIStoryboard(name: "IssueValidation", bundle: nil)
+                    let issueValidationVC = storyBoard.instantiateViewController(identifier: "IssueValidationViewController") as! IssueValidationViewController
+                    self.navigationController?.pushViewController(issueValidationVC, animated: true)
+                    
+                    issueValidationVC.paramIssueObject.append(solvedsList[indexPath.row])
+                }
+            }
+        }else if cell.issueStateLabel.text == "신고됨" {
+            
+        }
+        
     }
 }
